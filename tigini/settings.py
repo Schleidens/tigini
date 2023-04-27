b71a6d2f61bb1154891b053f9cb6d9f16fef4fc5
Schleidens <alectineschleidens2016@gmail.com>
2023-04-26 22:38:54 -0400

    refer the new user model and add settings for photo upload in media folder

--- tigini/settings.py
+++ tigini/settings.py
@@ -124,7 +124,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#add custom user model
+AUTH_USER_MODEL = 'authentication.User'
+
+#root for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
