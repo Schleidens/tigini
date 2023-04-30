58e82d33ae363bbb55c7b746101c699f53e77303
Schleidens <alectineschleidens2016@gmail.com>
2023-04-29 23:17:18 -0400

    specify form for password and add widget for other fields generated by the user model

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,13 +1,43 @@
 from django import forms
+from django.forms import TextInput, EmailInput
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 
 class createUserForm(UserCreationForm):
+    #specify password for having access to his widget
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        )
+    
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        )
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ['username', 'email', 'first_name', 'last_name']
+        
+        # add widgets for fields from user_model
+        widgets = {
+            'username' : TextInput(attrs={
+                'class' : 'form-control'
+            }),
+            
+            'email' : EmailInput(attrs={
+                'class' : 'form-control'
+            }),
+            
+            'first_name' : TextInput(attrs={
+                'class' : 'form-control'
+            }),
+            
+            'last_name' : TextInput(attrs={
+                'class' : 'form-control'
+            })
+        }
         
 
 #form for login user view
