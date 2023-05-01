74afd1385d8e3c7956e40e18c0d3e64a559b7c85
Schleidens <alectineschleidens2016@gmail.com>
2023-04-30 23:27:28 -0400

    set views and add templates for user profile view

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.views.generic import View
+
 # Create your views here.
+
+class user_profile(LoginRequiredMixin ,View):
+    template = 'user_profile.html'
+    
+    def get(self, request):
+        return render(self.request, self.template)
