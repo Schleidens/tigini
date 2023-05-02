fc67242a5164f7c9ce4bae5ab706f72682e05ced
Schleidens <alectineschleidens2016@gmail.com>
2023-05-02 15:15:43 -0400

    create blogPost model and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class blogPost(models.Model):
+    cover = models.ImageField(blank=True, null=True)
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now=True)
