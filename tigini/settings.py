753ed044ce630ea30c54f8b7b51053071e256854
Schleidens <alectineschleidens2016@gmail.com>
2023-05-08 13:10:06 -0400

    add custom password validation

--- tigini/settings.py
+++ tigini/settings.py
@@ -94,17 +94,17 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS':  {
+            'min_length': 8,
+        }
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'authentication.validators.containDigitValidator'
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        'NAME': 'authentication.validators.containLetterValidator'
+    }
 ]
 
 
